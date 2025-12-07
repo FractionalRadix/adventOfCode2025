a22bf1f54c7a6483401ac35ddb0850a0ddd522a4
Serge <nerostjust@gmail.com>
2025-12-07 09:26:03 +0100

    Add solution for day 7, part 1.

--- src/main/kotlin/solvers/Day07Solver.kt
+++ src/main/kotlin/solvers/Day07Solver.kt
@@ -1,20 +1,41 @@
 package com.cormontia.solvers
 
+import com.cormontia.utilities.Coor
+import com.cormontia.utilities.buildGridMap
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
 class Day07Solver {
     fun solve() {
-        val inputAsStrings: List<String> = Path("""src/main/resources/inputFiles/AoCDay07_sample.txt""").readLines()
+        val inputAsStrings: List<String> = Path("""src/main/resources/inputFiles/AoCDay07.txt""").readLines()
+        val grid = buildGridMap(inputAsStrings)
 
-        val answerPart1 = solvePart1()
+        val answerPart1 = solvePart1(grid) // 1570
         println(answerPart1)
         val answerPart2 = solvePart2()
         println(answerPart2)
     }
 
-    fun solvePart1(): Long {
-        return 0L
+    fun solvePart1(grid: MutableMap<Coor, Char>): Long {
+        val start = grid.keys.first { key -> grid[key] == 'S' }
+        val maxRows = grid.keys.maxBy { key -> key.row }.row
+        var splits = 0L
+        val beams = mutableSetOf<Long>()
+
+        beams.add(start.col)
+        for (row in start.row .. maxRows) {
+            val oldBeams = beams.toList()
+            for (beam in oldBeams) {
+                if (grid[Coor(row, beam)] == '^') {
+                    beams.remove(beam)
+                    beams.add(beam-1)
+                    beams.add(beam+1)
+                    splits++
+                }
+            }
+        }
+
+        return splits
     }
 
     fun solvePart2(): Long {
