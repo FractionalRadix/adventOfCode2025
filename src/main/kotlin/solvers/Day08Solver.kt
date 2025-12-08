a35c82570ea58bb8d10f9ff241beb0bb84744c15
Serge <nerostjust@gmail.com>
2025-12-08 19:59:25 +0100

    Clean up code.

--- src/main/kotlin/solvers/Day08Solver.kt
+++ src/main/kotlin/solvers/Day08Solver.kt
@@ -8,7 +8,13 @@ import kotlin.math.sqrt
 
 class Day08Solver {
     fun solve() {
-        val inputAsStrings: List<String> = Path("""src/main/resources/inputFiles/AoCDay08.txt""").readLines()
+
+        val useSample = false
+        val (cutoff, inputAsStrings) = if (useSample) {
+            Pair(10, Path("""src/main/resources/inputFiles/AoCDay08_sample.txt""").readLines())
+        } else {
+            Pair(1000, Path("""src/main/resources/inputFiles/AoCDay08.txt""").readLines())
+        }
 
         val list = mutableListOf<Coor3D>()
         for (line in inputAsStrings) {
@@ -19,13 +25,13 @@ class Day08Solver {
             list.add(Coor3D(x,y,z))
         }
 
-        //val answerPart1 = solvePart1(list) // 131150
-        //println(answerPart1)
+        val answerPart1 = solvePart1(list, cutoff) // 131150
+        println(answerPart1)
         val answerPart2 = solvePart2(list) // 2497445
         println(answerPart2)
     }
 
-    fun solvePart1(list: List<Coor3D>): Long {
+    fun solvePart1(list: List<Coor3D>, cutoff: Int): Long {
 
         val pairs = mutableListOf<Pair<Coor3D, Coor3D>>()
         //TODO?~ Use "forEachIndexed"
@@ -37,7 +43,6 @@ class Day08Solver {
             }
         }
 
-        val cutoff = 1000 // 10 for the sample input, 1000 for the real input
         val distances = pairs
             .map { (a,b) -> Triple(a, b, a.distance(b))}
             .sortedBy { (_, _, dist) -> dist }
@@ -48,7 +53,6 @@ class Day08Solver {
         val circuits = list.map { box -> setOf(box) }.toMutableSet()
         // For every pair, find the circuits that these boxes are in.
         for ((box1, box2, _) in distances) {
-            //println(pair)
             val circuit1 = circuits.first { circuit -> circuit.contains(box1) }
             val circuit2 = circuits.first { circuit -> circuit.contains(box2) }
             if (circuit1 != circuit2) {
@@ -70,7 +74,6 @@ class Day08Solver {
 
 
     fun solvePart2(list: List<Coor3D>): Long {
-        println("Making all pairs.")
         val pairs = mutableListOf<Pair<Coor3D, Coor3D>>()
         //TODO?~ Use "forEachIndexed"
         for (box1Index in list.indices) {
@@ -81,7 +84,6 @@ class Day08Solver {
             }
         }
 
-        println("Determining the distances between the pairs.")
         val distances = pairs
             .map { (a,b) -> Triple(a, b, a.distance(b))}
             .sortedBy { (_, _, dist) -> dist }
@@ -91,7 +93,6 @@ class Day08Solver {
         val circuits = list.map { box -> setOf(box) }.toMutableSet()
         // For every pair, find the circuits that these boxes are in.
         for ((box1, box2, _) in distances) {
-            //println(pair)
             val circuit1 = circuits.first { circuit -> circuit.contains(box1) }
             val circuit2 = circuits.first { circuit -> circuit.contains(box2) }
             if (circuit1 != circuit2) {
@@ -99,11 +100,8 @@ class Day08Solver {
                 newCircuit.addAll(circuit1)
                 newCircuit.addAll(circuit2)
                 if (newCircuit.size == list.size) {
-                    println("Done!")
                     val result = box1.x * box2.x
-                    println("$box1==$box1 box2==$box2 product of x coordinates is $result.")
                     return result
-
                 }
                 circuits.remove(circuit1)
                 circuits.remove(circuit2)
@@ -111,7 +109,7 @@ class Day08Solver {
             }
         }
 
-        //TODO!~
+        //TODO?~
         return 0L
     }
 }
